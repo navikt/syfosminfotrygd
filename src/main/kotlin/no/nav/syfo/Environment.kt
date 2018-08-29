e94215f1e79a78ded40dec27bd07d98945e027f6
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-08-29 15:23:05 +0200

    Correct environment variable names

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -3,8 +3,8 @@ package no.nav.syfo
 data class Environment(
         val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
         val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt(),
-        val srvsminfotrygdUsername: String = getEnvVar("SRVSMINFOTRYGD_USERNAME"),
-        val srvsminfotrygdPassword: String = getEnvVar("SRVSMINFOTRYGD_PASSWORD"),
+        val srvsminfotrygdUsername: String = getEnvVar("SRVSYFOSMINFOTRYGD_USERNAME"),
+        val srvsminfotrygdPassword: String = getEnvVar("SRVSYFOSMINFOTRYGD_PASSWORD"),
         val kafkaBootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL"),
         val syfoMottakInfotrygdRouteTopic: String = getEnvVar("SYFOMOTTAK_INFOTRYGD_ROUTE_TOPIC", "privat-syfomottak-sm2013-infotrygdRoute"),
         val syfoMottakOppgaveGsakInfotrygdTopic: String = getEnvVar("SYFOMOTTAK_OPPGAVE_GSAK_INFOTRYGD_TOPIC", "privat-syfomottak-sm2013-oppgaveGsakInfotrygd")
