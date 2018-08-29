9fbfb783a5c87b9988a3979430c333714fd511db
Kevin Sillerud <kevin.sillerud@gmail.com>
2018-08-29 14:36:59 +0200

    Add kluent import

--- src/test/kotlin/no/nav/syfo/KafkaITSpek.kt
+++ src/test/kotlin/no/nav/syfo/KafkaITSpek.kt
@@ -1,6 +1,7 @@
 package no.nav.syfo
 
 import no.nav.common.KafkaEnvironment
+import org.amshove.kluent.shouldEqual
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerRecord
