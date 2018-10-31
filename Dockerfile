00f29554ac7e3cbd3b3488016fdad191179a239c
Joakim kartveit <joakim.kartveit@nav.no>
2018-10-31 12:09:23 +0100

    Lagt til servicebruker i vault

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,4 @@ FROM navikt/java:11
 COPY build/install/* /app
 ENV JAVA_OPTS="'-Dlogback.configurationFile=logback-remote.xml'"
 ENTRYPOINT ["/app/bin/syfosminfotrygd"]
+ENV APPLICATION_PROFILE="remote"
--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -1,10 +1,34 @@
 package no.nav.syfo
 
+import com.natpryce.konfig.ConfigurationProperties
+import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
+import com.natpryce.konfig.EnvironmentVariables
+import com.natpryce.konfig.Key
+import com.natpryce.konfig.intType
+import com.natpryce.konfig.overriding
+import com.natpryce.konfig.stringType
+import java.io.File
+
+private const val vaultApplicationPropertiesPath = "/var/run/secrets/nais.io/vault/application.properties"
+
+private val config = if (System.getenv("APPLICATION_PROFILE") == "remote") {
+    systemProperties() overriding
+            EnvironmentVariables() overriding
+            ConfigurationProperties.fromFile(File(vaultApplicationPropertiesPath)) overriding
+            ConfigurationProperties.fromResource("application.properties")
+} else {
+    systemProperties() overriding
+            EnvironmentVariables() overriding
+            ConfigurationProperties.fromResource("application.properties")
+}
+
 data class Environment(
-    val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
-    val applicationThreads: Int = getEnvVar("APPLICATION_THREADS", "4").toInt(),
-    val srvsminfotrygdUsername: String = getEnvVar("SRVSYFOSMINFOTRYGD_USERNAME", "srvsminfotrygd"),
-    val srvsminfotrygdPassword: String = getEnvVar("SRVSYFOSMINFOTRYGD_PASSWORD", "srvsminfotrygdPassword"),
+    val isDevProfile: Boolean = config[Key("application.profile", stringType)] == "local",
+
+    val applicationPort: Int = config[Key("application.port", intType)],
+    val applicationThreads: Int = config[Key("application.threads", intType)],
+    val srvsminfotrygdUsername: String = config[Key("serviceuser.username", stringType)],
+    val srvsminfotrygdPassword: String = config[Key("serviceuser.password", stringType)],
     val kafkaBootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL", "SSL://12345.test.local:8443"),
     val sm2013AutomaticHandlingTopic: String = getEnvVar("SM2013_AUTOMATIC_HANDLING_TOPIC", "privat-syfo-sm2013-automatiskBehandling"),
     val smPaperAutomaticHandlingTopic: String = getEnvVar("SMPAPER_AUTOMATIC_HANDLING_TOPIC", "privat-syfo-smpapir-automatiskBehandling"),
