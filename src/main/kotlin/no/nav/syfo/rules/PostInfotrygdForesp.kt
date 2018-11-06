f1d236542796a535d36247f35cd250b0b2b4bd73
Joakim kartveit <joakim.kartveit@nav.no>
2018-11-06 08:50:32 +0100

    Flyttet regelene slik at den med laveste regelid er førtst

--- src/main/kotlin/no/nav/syfo/rules/PostInfotrygdForesp.kt
+++ src/main/kotlin/no/nav/syfo/rules/PostInfotrygdForesp.kt
@@ -14,69 +14,12 @@ data class RuleData(
 )
 
 enum class ValidationRules(override val ruleId: Int?, override val status: Status, override val predicate: (RuleData) -> Boolean) : Rule<RuleData> {
-    @Description("Personen har flyttet ( stanskode FL i Infotrygd)")
-    PERSON_MOVING_KODE_FL(1546, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
-        infotrygdForesp.sMhistorikk?.sykmelding?.any { sykmelding ->
-            sykmelding?.periode?.stans == "FL"
-        } ?: false
-        }),
-
-    @Description("Hvis personen har stanskode DØD i Infotrygd")
-    PATIENT_DEAD(1545, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
-        infotrygdForesp.sMhistorikk?.sykmelding?.any { sykmelding ->
-            sykmelding?.periode?.stans == "DØD"
-        } ?: false
-    }),
 
     @Description("Hvis pasienten ikke finnes i infotrygd")
     PATIENT_NOT_IN_IP(1501, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
         when (infotrygdForesp.pasient?.isFinnes) {
             null -> false
             else -> !infotrygdForesp.pasient.isFinnes
-        }
-    }),
-
-    @Description("Hvis ny friskmeldingsdato er mindre enn utbetalingTOM registrert i Infotrygd")
-    NEW_CLEAN_BILL_DATE_BEFORE_PAYOUT(1517, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
-        val sMhistorikkfriskmeldtDato: java.time.LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.friskmeldtDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-        val sMhistorikkutbetTOM: java.time.LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.utbetTOM?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-
-        when (sMhistorikkutbetTOM) {
-            null -> false
-            else -> sMhistorikkfriskmeldtDato?.isBefore(sMhistorikkutbetTOM) ?: false
-        }
-    }),
-
-    @Description("Hvis ny friskmeldingsdato er mindre enn arbuforTOM registrert i Infotrygd")
-    NEW_CLEAN_BILL_DATE_BEFORE_ARBUFORTOM(1516, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
-        val sMhistorikkfriskmeldtDato: LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.friskmeldtDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-        val sMhistorikkArbuforTom: LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.arbufoerTOM?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-
-        when (sMhistorikkArbuforTom) {
-            null -> false
-            else -> sMhistorikkfriskmeldtDato?.isBefore(sMhistorikkArbuforTom) ?: false
-        }
-    }),
-
-    @Description("Hvis ny friskmeldingsdato er tidligere enn registrert friskmeldingsdato i Infotrygd")
-    NEW_CLEAN_BILL_DATE_BEFORE_REGISTERD_CLEAN_BILL_DATE(1518, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
-        val newfriskmeldtDato: LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.friskmeldtDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-        val secoundfriskmeldtDato: LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.drop(1)?.firstOrNull()?.periode?.friskmeldtDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-
-        when (secoundfriskmeldtDato) {
-            null -> false
-            else -> newfriskmeldtDato?.isAfter(secoundfriskmeldtDato) ?: false
-        }
-    }),
-
-    @Description("Hvis delvis sammenfallende sykmeldingsperiode er registrert i Infotrygd")
-    PARTIALLY_COINCIDENT_SICK_LEAVE_PERIOD_WITH_PREVIOUSLY_REGISTERED_SICK_LEAVE(1513, Status.MANUAL_PROCESSING, { (infotrygdForesp, healthInformation) ->
-        val healthInformationPeriodeFomdato: LocalDate? = healthInformation.aktivitet.periode.firstOrNull()?.periodeFOMDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-        val infotrygdforespArbuforFom: LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.arbufoerFOM?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-
-        when (infotrygdforespArbuforFom) {
-            null -> false
-            else -> healthInformationPeriodeFomdato?.isBefore(infotrygdforespArbuforFom) ?: false
         }
     }),
 
@@ -91,6 +34,25 @@ enum class ValidationRules(override val ruleId: Int?, override val status: Statu
         } else {
             false
         }
+    }),
+
+    @Description("Hvis delvis sammenfallende sykmeldingsperiode er registrert i Infotrygd")
+    PARTIALLY_COINCIDENT_SICK_LEAVE_PERIOD_WITH_PREVIOUSLY_REGISTERED_SICK_LEAVE(1513, Status.MANUAL_PROCESSING, { (infotrygdForesp, healthInformation) ->
+        val healthInformationPeriodeFomdato: LocalDate? = healthInformation.aktivitet.periode.firstOrNull()?.periodeFOMDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+        val infotrygdforespArbuforFom: LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.arbufoerFOM?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+
+        when (infotrygdforespArbuforFom) {
+            null -> false
+            else -> healthInformationPeriodeFomdato?.isBefore(infotrygdforespArbuforFom) ?: false
+        }
+    }),
+
+    @Description("Hvis sykmeldingsperioden er større enn 1 år")
+    SICK_LEAVE_PERIOD_OVER_1_YEAR(1514, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
+        val sMhistorikkArbuforFOM: java.time.LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.arbufoerFOM?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+        val sMhistorikkArbuforTOM: java.time.LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.arbufoerTOM?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+
+        sMhistorikkArbuforFOM != null && sMhistorikkArbuforTOM != null && java.time.temporal.ChronoUnit.YEARS.between(sMhistorikkArbuforFOM, sMhistorikkArbuforTOM) >= 1
     }),
 
     @Description("Hvis sykmeldingen er forlengelse av registrert sykepengehistorikk fra annet kontor så medlingen gå til manuell behandling slik at  saksbehandler kan registrere sykepengetilfellet på ny identdato og  send oppgave til Nav forvaltning for registrering av inntektsopplysninger")
@@ -110,6 +72,57 @@ enum class ValidationRules(override val ruleId: Int?, override val status: Statu
         }
     }),
 
+    @Description("Hvis ny friskmeldingsdato er mindre enn arbuforTOM registrert i Infotrygd")
+    NEW_CLEAN_BILL_DATE_BEFORE_ARBUFORTOM(1516, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
+        val sMhistorikkfriskmeldtDato: LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.friskmeldtDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+        val sMhistorikkArbuforTom: LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.arbufoerTOM?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+
+        when (sMhistorikkArbuforTom) {
+            null -> false
+            else -> sMhistorikkfriskmeldtDato?.isBefore(sMhistorikkArbuforTom) ?: false
+        }
+    }),
+
+    @Description("Hvis ny friskmeldingsdato er mindre enn utbetalingTOM registrert i Infotrygd")
+    NEW_CLEAN_BILL_DATE_BEFORE_PAYOUT(1517, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
+        val sMhistorikkfriskmeldtDato: java.time.LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.friskmeldtDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+        val sMhistorikkutbetTOM: java.time.LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.utbetTOM?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+
+        when (sMhistorikkutbetTOM) {
+            null -> false
+            else -> sMhistorikkfriskmeldtDato?.isBefore(sMhistorikkutbetTOM) ?: false
+        }
+    }),
+
+    @Description("Hvis ny friskmeldingsdato er tidligere enn registrert friskmeldingsdato i Infotrygd")
+    NEW_CLEAN_BILL_DATE_BEFORE_REGISTERD_CLEAN_BILL_DATE(1518, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
+        val newfriskmeldtDato: LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.friskmeldtDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+        val secoundfriskmeldtDato: LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.drop(1)?.firstOrNull()?.periode?.friskmeldtDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+
+        when (secoundfriskmeldtDato) {
+            null -> false
+            else -> newfriskmeldtDato?.isAfter(secoundfriskmeldtDato) ?: false
+        }
+    }),
+
+    @Description("Hvis en sykmelding fra manuellterapeut overstiger 12 uker regnet fra første sykefraværsdag")
+    DOCTOR_IS_KI_AND_OVER_12_WEEKS(1519, Status.INVALID, { (infotrygdForesp, healthInformation) ->
+        val samhandlerType = infotrygdForesp.behandlerInfo?.behandler?.firstOrNull()?.mottakerKode
+        val healthInformationPeriodeFomdato: LocalDate? = healthInformation.aktivitet.periode.firstOrNull()?.periodeFOMDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+        val healthInformationPeriodeTomdato: LocalDate? = healthInformation.aktivitet.periode.firstOrNull()?.periodeTOMDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+
+        samhandlerType == TypeMottakerKode.KI && java.time.temporal.ChronoUnit.DAYS.between(healthInformationPeriodeFomdato, healthInformationPeriodeTomdato) > 84
+    }),
+
+    @Description("Hvis en sykmelding fra manuellterapeut overstiger 12 uker regnet fra første sykefraværsdag")
+    DOCTOR_IS_MT_AND_OVER_12_WEEKS(1520, Status.INVALID, { (infotrygdForesp, healthInformation) ->
+        val samhandlerType = infotrygdForesp.behandlerInfo?.behandler?.firstOrNull()?.mottakerKode
+        val healthInformationPeriodeFomdato: LocalDate? = healthInformation.aktivitet.periode.firstOrNull()?.periodeFOMDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+        val healthInformationPeriodeTomdato: java.time.LocalDate? = healthInformation.aktivitet.periode.firstOrNull()?.periodeTOMDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+
+        samhandlerType == TypeMottakerKode.MT && java.time.temporal.ChronoUnit.DAYS.between(healthInformationPeriodeFomdato, healthInformationPeriodeTomdato) > 84
+    }),
+
     @Description("Hvis uføregrad er endret")
     DIABILITY_GRADE_CANGED(1530, Status.MANUAL_PROCESSING, { (infotrygdForesp, healthInformation) ->
         val disabilityGradeIT: kotlin.Int? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.ufoeregrad?.toInt()
@@ -121,21 +134,6 @@ enum class ValidationRules(override val ruleId: Int?, override val status: Statu
         disabilityGradeIT != healthInformationDisabilityGrade &&
                 sMhistorikkArbuforFOM?.isAfter(healthInformationPeriodeFOMDato) ?: false &&
                 sMhistorikkArbuforFOM?.isBefore(healthInformationPeriodeTOMDato) ?: false
-    }),
-
-    @Description("Infotrygd returnerte en feil, vi kan ikke automatisk oppdatere Infotrygd")
-    ERROR_FROM_IT(1591, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
-        val hovedStatusKodemelding: Int? = infotrygdForesp.hovedStatus.kodeMelding.toIntOrNull()
-        val sMhistorikktStatusKodemelding: Int? = infotrygdForesp.sMhistorikk?.status?.kodeMelding?.toIntOrNull()
-        val parallelleYtelserStatusKodemelding: kotlin.Int? = infotrygdForesp.parallelleYtelser?.status?.kodeMelding?.toIntOrNull()
-        val diagnoseOKUttrekkStatusKodemelding: kotlin.Int? = infotrygdForesp.diagnosekodeOK?.status?.kodeMelding?.toIntOrNull()
-        val pasientUttrekkStatusKodemelding: kotlin.Int? = infotrygdForesp.pasient?.status?.kodeMelding?.toIntOrNull()
-
-        hovedStatusKodemelding ?: 0 > 4 ||
-                sMhistorikktStatusKodemelding ?: 0 > 4 ||
-                parallelleYtelserStatusKodemelding ?: 0 > 4 ||
-                diagnoseOKUttrekkStatusKodemelding ?: 0 > 4 ||
-                pasientUttrekkStatusKodemelding ?: 0 > 4
     }),
 
     @Description("Hvis forlengelse utover registrert tiltak FA tiltak")
@@ -150,6 +148,27 @@ enum class ValidationRules(override val ruleId: Int?, override val status: Statu
         }?.tilltak?.tom?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
 
         sMhistorikkTilltakTypeFA ?: false && healthInformationPeriodeFomdato?.isAfter(sMhistorikkTilltakTypeFATomDato) ?: false
+    }),
+
+    @Description("Hvis personen har stanskode DØD i Infotrygd")
+    PATIENT_DEAD(1545, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
+        infotrygdForesp.sMhistorikk?.sykmelding?.any { sykmelding ->
+            sykmelding?.periode?.stans == "DØD"
+        } ?: false
+    }),
+
+    @Description("Personen har flyttet ( stanskode FL i Infotrygd)")
+    PERSON_MOVING_KODE_FL(1546, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
+        infotrygdForesp.sMhistorikk?.sykmelding?.any { sykmelding ->
+            sykmelding?.periode?.stans == "FL"
+        } ?: false
+        }),
+
+    @Description("Hvis perioden er avsluttet-død(AD)")
+    PERIOD_ENDED_DEAD(1548, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
+        infotrygdForesp.sMhistorikk?.sykmelding?.any { sykemelding ->
+            sykemelding?.periode?.stans == "AD"
+        } ?: false
     }),
 
     @Description("Hvis perioden er avsluttet (AA)")
@@ -168,13 +187,6 @@ enum class ValidationRules(override val ruleId: Int?, override val status: Statu
         }
     }),
 
-    @Description("Hvis perioden er avsluttet-død(AD)")
-    PERIOD_ENDED_DEAD(1548, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
-        infotrygdForesp.sMhistorikk?.sykmelding?.any { sykemelding ->
-            sykemelding?.periode?.stans == "AD"
-        } ?: false
-    }),
-
     @Description("Hvis maks sykepenger er utbetalt")
     MAX_SICK_LEAVE_PAYOUT(1551, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
         infotrygdForesp.sMhistorikk?.sykmelding?.any { sykemelding ->
@@ -189,29 +201,25 @@ enum class ValidationRules(override val ruleId: Int?, override val status: Statu
         } ?: false
     }),
 
-    @Description("Hvis sykmeldingsperioden er større enn 1 år")
-    SICK_LEAVE_PERIOD_OVER_1_YEAR(1514, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
-        val sMhistorikkArbuforFOM: java.time.LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.arbufoerFOM?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-        val sMhistorikkArbuforTOM: java.time.LocalDate? = infotrygdForesp.sMhistorikk?.sykmelding?.firstOrNull()?.periode?.arbufoerTOM?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
+    @Description("Infotrygd returnerte en feil, vi kan ikke automatisk oppdatere Infotrygd")
+    ERROR_FROM_IT(1591, Status.MANUAL_PROCESSING, { (infotrygdForesp) ->
+        val hovedStatusKodemelding: Int? = infotrygdForesp.hovedStatus.kodeMelding.toIntOrNull()
+        val sMhistorikktStatusKodemelding: Int? = infotrygdForesp.sMhistorikk?.status?.kodeMelding?.toIntOrNull()
+        val parallelleYtelserStatusKodemelding: kotlin.Int? = infotrygdForesp.parallelleYtelser?.status?.kodeMelding?.toIntOrNull()
+        val diagnoseOKUttrekkStatusKodemelding: kotlin.Int? = infotrygdForesp.diagnosekodeOK?.status?.kodeMelding?.toIntOrNull()
+        val pasientUttrekkStatusKodemelding: kotlin.Int? = infotrygdForesp.pasient?.status?.kodeMelding?.toIntOrNull()
 
-        sMhistorikkArbuforFOM != null && sMhistorikkArbuforTOM != null && java.time.temporal.ChronoUnit.YEARS.between(sMhistorikkArbuforFOM, sMhistorikkArbuforTOM) >= 1
+        hovedStatusKodemelding ?: 0 > 4 ||
+                sMhistorikktStatusKodemelding ?: 0 > 4 ||
+                parallelleYtelserStatusKodemelding ?: 0 > 4 ||
+                diagnoseOKUttrekkStatusKodemelding ?: 0 > 4 ||
+                pasientUttrekkStatusKodemelding ?: 0 > 4
     }),
-
-    @Description("Hvis en sykmelding fra manuellterapeut overstiger 12 uker regnet fra første sykefraværsdag")
-    DOCTOR_IS_MT_AND_OVER_12_WEEKS(1520, Status.INVALID, { (infotrygdForesp, healthInformation) ->
-        val samhandlerType = infotrygdForesp.behandlerInfo?.behandler?.firstOrNull()?.mottakerKode
-        val healthInformationPeriodeFomdato: LocalDate? = healthInformation.aktivitet.periode.firstOrNull()?.periodeFOMDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-        val healthInformationPeriodeTomdato: java.time.LocalDate? = healthInformation.aktivitet.periode.firstOrNull()?.periodeTOMDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-
-        samhandlerType == TypeMottakerKode.MT && java.time.temporal.ChronoUnit.DAYS.between(healthInformationPeriodeFomdato, healthInformationPeriodeTomdato) > 84
-    }),
-
-    @Description("Hvis en sykmelding fra manuellterapeut overstiger 12 uker regnet fra første sykefraværsdag")
-    DOCTOR_IS_KI_AND_OVER_12_WEEKS(1519, Status.INVALID, { (infotrygdForesp, healthInformation) ->
-        val samhandlerType = infotrygdForesp.behandlerInfo?.behandler?.firstOrNull()?.mottakerKode
-        val healthInformationPeriodeFomdato: LocalDate? = healthInformation.aktivitet.periode.firstOrNull()?.periodeFOMDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-        val healthInformationPeriodeTomdato: LocalDate? = healthInformation.aktivitet.periode.firstOrNull()?.periodeTOMDato?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDate()
-
-        samhandlerType == TypeMottakerKode.KI && java.time.temporal.ChronoUnit.DAYS.between(healthInformationPeriodeFomdato, healthInformationPeriodeTomdato) > 84
-    })
 }
+
+/* TODO only count work days
+fun workdaysBetween(a: LocalDate, b: LocalDate): Int = (1..(ChronoUnit.DAYS.between(a, b) - 1))
+        .map { a.plusDays(it) }
+        .filter { it.dayOfWeek !in arrayOf(DayOfWeek.SATURDAY, DayOfWeek.SUNDAY) }
+        .count()
+*/
