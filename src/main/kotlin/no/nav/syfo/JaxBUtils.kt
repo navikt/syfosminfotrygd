6765bbe3ec2776eb7bf08aa3b2573b380231464f
Joakim kartveit <joakim.kartveit@nav.no>
2019-02-12 14:19:33 +0100

    Added KontrollsystemBlokkType::class.java, KontrollSystemBlokk::class.java, InfotrygdForesp::class.java to the fellesformat unmarsheller

--- src/main/kotlin/no/nav/syfo/JaxBUtils.kt
+++ src/main/kotlin/no/nav/syfo/JaxBUtils.kt
@@ -3,6 +3,8 @@ package no.nav.syfo
 import no.kith.xmlstds.msghead._2006_05_24.XMLMsgHead
 import no.nav.helse.infotrygd.foresp.InfotrygdForesp
 import no.nav.helse.sm2013.HelseOpplysningerArbeidsuforhet
+import no.nav.helse.sm2013.KontrollSystemBlokk
+import no.nav.helse.sm2013.KontrollsystemBlokkType
 import no.trygdeetaten.xml.eiff._1.XMLEIFellesformat
 import no.trygdeetaten.xml.eiff._1.XMLMottakenhetBlokk
 
@@ -18,8 +20,9 @@ val infotrygdSporringMarshaller: Marshaller = infotrygdSporringJaxBContext.creat
 
 val infotrygdSporringUnmarshaller: Unmarshaller = infotrygdSporringJaxBContext.createUnmarshaller()
 
-val fellesformatJaxBContext: JAXBContext = JAXBContext.newInstance(XMLEIFellesformat::class.java, XMLMsgHead::class.java,
-        XMLMottakenhetBlokk::class.java, HelseOpplysningerArbeidsuforhet::class.java)
+val fellesformatJaxBContext: JAXBContext = JAXBContext.newInstance(XMLEIFellesformat::class.java,
+        XMLMsgHead::class.java, XMLMottakenhetBlokk::class.java, HelseOpplysningerArbeidsuforhet::class.java,
+        KontrollsystemBlokkType::class.java, KontrollSystemBlokk::class.java, InfotrygdForesp::class.java)
 val fellesformatUnmarshaller: Unmarshaller = fellesformatJaxBContext.createUnmarshaller()
 
 val fellesformatMarshaller: Marshaller = fellesformatJaxBContext.createMarshaller()
