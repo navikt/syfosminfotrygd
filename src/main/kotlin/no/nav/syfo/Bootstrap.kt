33e2fcbb476bf3c479b6d9e737b8dbcbc849b148
Joakim kartveit <joakim.kartveit@nav.no>
2019-02-15 16:27:26 +0100

    Fixed when == null, then log error

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -312,7 +312,7 @@ suspend fun CoroutineScope.produceManualTask(kafkaProducer: KafkaProducer<String
 
     val navKontor = fetchNAVKontor(organisasjonEnhetV2, geografiskTilknytning.await()).await()
 
-    when (navKontor.enhetId != null) {
+    when (navKontor.enhetId == null) {
         true -> log.error("Nav kontor is null, where to send the task to??? $logKeys", *logValues) // TODO
         else -> createTask(kafkaProducer, receivedSykmelding, results, navKontor, logKeys, logValues)
     }
