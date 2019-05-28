bb47da46cb3ceed38cdea5e5f2d194d1ff155658
Tia Firing <tia.firing@nav.no>
2019-05-28 13:35:30 +0200

    Nytt forsøk på å rette prodfeil

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -409,7 +409,7 @@ fun findOperasjonstype(
             typeSMinfo.periode.arbufoerTOM != null && periode.periodeFOMDato.isEqual(typeSMinfo.periode.arbufoerTOM) ||
             (typeSMinfo.periode.arbufoerTOM != null && periode.periodeFOMDato.isAfter(typeSMinfo.periode.arbufoerTOM))) {
         2
-    } else if (typeSMinfo.periode.arbufoerFOM == periode.periodeFOMDato) {
+    } else if (typeSMinfo.periode.arbufoerFOM == periode.periodeFOMDato || (typeSMinfo.periode.arbufoerFOM != null && typeSMinfo.periode.arbufoerFOM.isBefore(periode.periodeFOMDato))) {
         3
     } else {
         log.error("Could not determined operasjonstype $logKeys", *logValues)
