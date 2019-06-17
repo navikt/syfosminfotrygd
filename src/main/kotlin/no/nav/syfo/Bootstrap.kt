1c607f59c918c66ad38a6d8908967cbf9646a955
Joakim Kartveit <joakimkartveit@gmail.com>
2019-06-17 13:54:53 +0200

    Deployer versjon 1.0.35

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -347,7 +347,6 @@ fun sendInfotrygdOppdateringMq(
 ) = producer.send(session.createTextMessage().apply {
     log.info("Message has oprasjonstype: {}  $logKeys", fellesformat.get<KontrollsystemBlokkType>().infotrygdBlokk.first().operasjonstype, *logValues)
     text = xmlObjectWriter.writeValueAsString(fellesformat)
-    log.info("Infotrygd xml: ${xmlObjectWriter.writeValueAsString(fellesformat)}")
     log.info("Message is sendt to infotrygd $logKeys", *logValues)
 })
 
