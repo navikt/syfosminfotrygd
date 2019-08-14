082b1cebf8cf3110b40c35f25240da62e878b773
Joakim Kartveit <joakimkartveit@gmail.com>
2019-08-14 08:46:21 +0200

    Klargjør for å håndere eventuele http 404

--- src/main/kotlin/no/nav/syfo/client/Norg2Client.kt
+++ src/main/kotlin/no/nav/syfo/client/Norg2Client.kt
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.HttpClient
+import io.ktor.client.call.receive
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.request.accept
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import io.ktor.client.response.HttpResponse
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.util.KtorExperimentalAPI
@@ -33,13 +35,14 @@ class Norg2Client(
 
     suspend fun getLocalNAVOffice(geografiskOmraade: String, diskresjonskode: String?): Enhet =
             retry("find_local_nav_office") {
-                client.get<Enhet>("$endpointUrl/enhet/navkontor/$geografiskOmraade") {
+                val httpResponse = client.get<HttpResponse>("$endpointUrl/enhet/navkontor/$geografiskOmraade") {
                     accept(ContentType.Application.Json)
                     contentType(ContentType.Application.Json)
                     if (!diskresjonskode.isNullOrEmpty()) {
                         parameter("disk", diskresjonskode)
                     }
                 }
+                httpResponse.call.response.receive<Enhet>()
             }
 }
 
