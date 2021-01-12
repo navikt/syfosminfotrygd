6dcd808ecd46986dd3bab8c835aeb79a7798054f
Tia Firing <tia.firing@nav.no>
2021-01-12 11:33:06 +0100

    fjerner spesialbehandling for reisetilsk/behandlingsd

--- src/test/kotlin/no/nav/syfo/services/OpprettOppgaveTest.kt
+++ src/test/kotlin/no/nav/syfo/services/OpprettOppgaveTest.kt
@@ -8,9 +8,6 @@ import java.time.LocalDate
 import java.util.UUID
 import kotlinx.coroutines.runBlocking
 import no.nav.syfo.client.SyfosmreglerClient
-import no.nav.syfo.generatePeriode
-import no.nav.syfo.generateSykmelding
-import no.nav.syfo.model.Gradert
 import no.nav.syfo.model.RuleInfo
 import no.nav.syfo.model.Status
 import no.nav.syfo.model.ValidationResult
@@ -31,102 +28,12 @@ object OpprettOppgaveTest : Spek({
     )))
     val syfosmreglerClient = mockk<SyfosmreglerClient>()
 
-    val receivedSykmeldingMedBehandlingsdager = receivedSykmelding(
-        id = UUID.randomUUID().toString(),
-        sykmelding = generateSykmelding(perioder = listOf(
-            generatePeriode(
-                fom = LocalDate.now(),
-                tom = LocalDate.now().plusMonths(3).plusDays(1),
-                behandlingsdager = 1
-            )
-        )))
-    val receivedSykmeldingMedReisetilskudd = receivedSykmelding(
-        id = UUID.randomUUID().toString(),
-        sykmelding = generateSykmelding(perioder = listOf(
-            generatePeriode(
-                fom = LocalDate.now(),
-                tom = LocalDate.now().plusMonths(3).plusDays(1),
-                reisetilskudd = true
-            )
-        )))
-
     beforeEachTest {
         clearAllMocks()
         coEvery { syfosmreglerClient.executeRuleValidation(any(), any()) } returns ValidationResult(Status.OK, emptyList())
     }
 
     describe("Oppretter manuelle oppgaver med riktige parametre") {
-        it("Behandlingstema er ANY hvis sykmelding ikke har behandlingsdager eller reisetilskudd") {
-            val receivedSykmelding = receivedSykmelding(UUID.randomUUID().toString())
-
-            runBlocking {
-                val oppgave = opprettProduceTask(syfosmreglerClient, receivedSykmelding, validationResults, "dev-fss", LocalDate.of(2021, 1, 10), loggingMeta)
-
-                oppgave.behandlingstema shouldEqual "ANY"
-            }
-        }
-        it("Behandlingstema er ANY hvis sykmelding har behandlingsdager og miljø er prod og nå er før 31/12 2020") {
-            runBlocking {
-                val oppgave = opprettProduceTask(syfosmreglerClient, receivedSykmeldingMedBehandlingsdager, validationResults, "prod-fss", LocalDate.of(2020, 12, 10), loggingMeta)
-
-                oppgave.behandlingstema shouldEqual "ANY"
-            }
-        }
-        it("Behandlingstema er ab0351 hvis sykmelding har behandlingsdager og miljø er prod og nå er etter 31/12 2020") {
-            runBlocking {
-                val oppgave = opprettProduceTask(syfosmreglerClient, receivedSykmeldingMedBehandlingsdager, validationResults, "prod-fss", LocalDate.of(2021, 1, 1), loggingMeta)
-
-                oppgave.behandlingstema shouldEqual "ab0351"
-            }
-        }
-        it("Behandlingstema er ab0351 hvis sykmelding har behandlingsdager og miljø er dev") {
-            runBlocking {
-                val oppgave = opprettProduceTask(syfosmreglerClient, receivedSykmeldingMedBehandlingsdager, validationResults, "dev-fss", LocalDate.of(2020, 12, 10), loggingMeta)
-
-                oppgave.behandlingstema shouldEqual "ab0351"
-            }
-        }
-        it("Behandlingstema er ANY hvis sykmelding har reisetilskudd og miljø er prod og nå er før 31/12 2020") {
-            runBlocking {
-                val oppgave = opprettProduceTask(syfosmreglerClient, receivedSykmeldingMedReisetilskudd, validationResults, "prod-fss", LocalDate.of(2020, 12, 10), loggingMeta)
-
-                oppgave.behandlingstema shouldEqual "ANY"
-            }
-        }
-        it("Behandlingstema er ab0237 hvis sykmelding har reisetilskudd og miljø er prod og nå er etter 31/12 2020") {
-            runBlocking {
-                val oppgave = opprettProduceTask(syfosmreglerClient, receivedSykmeldingMedReisetilskudd, validationResults, "prod-fss", LocalDate.of(2021, 1, 1), loggingMeta)
-
-                oppgave.behandlingstema shouldEqual "ab0237"
-            }
-        }
-        it("Behandlingstema er ab0237 hvis sykmelding har reisetilskudd og miljø er dev") {
-            runBlocking {
-                val oppgave = opprettProduceTask(syfosmreglerClient, receivedSykmeldingMedReisetilskudd, validationResults, "dev-fss", LocalDate.of(2020, 12, 10), loggingMeta)
-
-                oppgave.behandlingstema shouldEqual "ab0237"
-            }
-        }
-        it("Behandlingstema er ab0237 hvis sykmelding er gradert med reisetilskudd og miljø er prod og nå er etter 31/12 2020") {
-            val receivedSykmeldingGradertMedReisetilskudd = receivedSykmelding(
-                id = UUID.randomUUID().toString(),
-                sykmelding = generateSykmelding(perioder = listOf(
-                    generatePeriode(
-                        fom = LocalDate.now(),
-                        tom = LocalDate.now().plusMonths(3).plusDays(1),
-                        gradert = Gradert(
-                            reisetilskudd = true,
-                            grad = 90
-                        )
-                    )
-                )))
-
-            runBlocking {
-                val oppgave = opprettProduceTask(syfosmreglerClient, receivedSykmeldingGradertMedReisetilskudd, validationResults, "prod-fss", LocalDate.of(2021, 1, 1), loggingMeta)
-
-                oppgave.behandlingstema shouldEqual "ab0237"
-            }
-        }
         it("Behandlingstype er ae0256 hvis sykmelding treffer manuell-regler") {
             coEvery { syfosmreglerClient.executeRuleValidation(any(), any()) } returns ValidationResult(Status.MANUAL_PROCESSING, listOf(
                 RuleInfo("TILBAKEDATERT_MER_ENN_8_DAGER_FORSTE_SYKMELDING_MED_BEGRUNNELSE", "message for sender", "message for user", Status.MANUAL_PROCESSING)))
