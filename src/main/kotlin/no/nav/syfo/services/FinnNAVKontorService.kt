73e854996fa38b8ad71d32a4a09cf2313c30dd5b
Tia Firing <tia.firing@nav.no>
2021-01-21 09:24:24 +0100

    logger GT og diskresjonskode (kun test)

--- src/main/kotlin/no/nav/syfo/services/FinnNAVKontorService.kt
+++ src/main/kotlin/no/nav/syfo/services/FinnNAVKontorService.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.services
 
 import io.ktor.util.KtorExperimentalAPI
 import no.nav.syfo.client.Norg2Client
+import no.nav.syfo.log
 import no.nav.syfo.pdl.service.PdlPersonService
 import no.nav.syfo.util.LoggingMeta
 
@@ -12,6 +13,7 @@ class FinnNAVKontorService(
 ) {
     suspend fun finnLokaltNavkontor(fnr: String, loggingMeta: LoggingMeta): String {
         val pdlPerson = pdlPersonService.getPerson(fnr, loggingMeta)
+        log.info("Fant GT=${pdlPerson.gt} og diskresjonskode ${pdlPerson.adressebeskyttelse}")
         return norg2Client.getLocalNAVOffice(pdlPerson.gt, pdlPerson.adressebeskyttelse).enhetNr
     }
 }
