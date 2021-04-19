9154107848ed77738f403c8636dd8c7a81241a57
Tia Firing <tia.firing@nav.no>
2021-04-19 12:39:54 +0200

    logg

--- src/main/kotlin/no/nav/syfo/services/FinnNAVKontorService.kt
+++ src/main/kotlin/no/nav/syfo/services/FinnNAVKontorService.kt
@@ -1,7 +1,9 @@
 package no.nav.syfo.services
 
 import io.ktor.util.KtorExperimentalAPI
+import net.logstash.logback.argument.StructuredArguments
 import no.nav.syfo.client.Norg2Client
+import no.nav.syfo.log
 import no.nav.syfo.pdl.service.PdlPersonService
 import no.nav.syfo.util.LoggingMeta
 
@@ -12,6 +14,7 @@ class FinnNAVKontorService(
 ) {
     suspend fun finnLokaltNavkontor(fnr: String, loggingMeta: LoggingMeta): String {
         val pdlPerson = pdlPersonService.getPerson(fnr, loggingMeta)
+        log.info("PDL returnerte diskresjonskode {} og GT {}, {}", pdlPerson.adressebeskyttelse, pdlPerson.gt, StructuredArguments.fields(loggingMeta))
         return norg2Client.getLocalNAVOffice(pdlPerson.gt, pdlPerson.adressebeskyttelse).enhetNr
     }
 }
