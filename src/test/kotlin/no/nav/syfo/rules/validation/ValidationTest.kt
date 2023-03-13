1f4469525ff0a721e42a33d231a78aa99194c97e
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-03-13 14:15:44 +0100

    Added in more unit tests for validation

--- src/test/kotlin/no/nav/syfo/rules/validation/ValidationTest.kt
+++ src/test/kotlin/no/nav/syfo/rules/validation/ValidationTest.kt
@@ -11,6 +11,7 @@ import no.nav.syfo.receivedSykmelding
 import org.amshove.kluent.shouldBeEqualTo
 import java.time.LocalDate
 import java.util.UUID
+import no.nav.syfo.model.Gradert
 
 class ValidationTest : FunSpec({
 
@@ -95,6 +96,155 @@ class ValidationTest : FunSpec({
             result.first.treeResult.status shouldBeEqualTo Status.OK
         }
 
-        // TODO add in test for all rules
+        test("should trigger rule NUMBER_OF_TREATMENT_DAYS_SET, should be MANUAL_PROCESSING") {
+            val sykmelding = generateSykmelding(
+                perioder = listOf(
+                    generatePeriode(
+                        fom = LocalDate.now(),
+                        tom = LocalDate.now().plusDays(10),
+                        behandlingsdager = 1
+                    )
+                )
+            )
+
+            val infotrygdForespResponse = InfotrygdForesp().apply {
+                hovedStatus = StatusType().apply {
+                    kodeMelding = "00"
+                }
+            }
+
+            val receivedSykmelding = receivedSykmelding(
+                id = UUID.randomUUID().toString(),
+                sykmelding = sykmelding,
+                fellesformat = ""
+            )
+
+            val ruleMetadata = RuleMetadata(
+                receivedDate = receivedSykmelding.mottattDato,
+                signatureDate = receivedSykmelding.sykmelding.signaturDato,
+                patientPersonNumber = receivedSykmelding.personNrPasient,
+                rulesetVersion = receivedSykmelding.rulesetVersion,
+                legekontorOrgnr = receivedSykmelding.legekontorOrgNr,
+                tssid = receivedSykmelding.tssid,
+                infotrygdForesp = infotrygdForespResponse
+            )
+
+            val result = ruleTree.runRules(sykmelding, ruleMetadata)
+
+            result.first.rulePath.map { it.rule to it.ruleResult } shouldBeEqualTo listOf(
+                ValidationRules.NUMBER_OF_TREATMENT_DAYS_SET to true
+            )
+
+            mapOf(
+                "perioder" to sykmelding.perioder
+            ) shouldBeEqualTo result.first.ruleInputs
+
+            result.first.treeResult.ruleHit shouldBeEqualTo ValidationRuleHit.NUMBER_OF_TREATMENT_DAYS_SET.ruleHit
+
+            result.first.treeResult.status shouldBeEqualTo Status.MANUAL_PROCESSING
+        }
+
+        test("should trigger rule GRADERT_REISETILSKUDD_ER_OPPGITT, should be MANUAL_PROCESSING") {
+            val sykmelding = generateSykmelding(
+                perioder = listOf(
+                    generatePeriode(
+                        fom = LocalDate.now(),
+                        tom = LocalDate.now().plusDays(10),
+                        gradert = Gradert(
+                            reisetilskudd = true,
+                            grad = 90
+                        )
+                    )
+                )
+            )
+
+            val infotrygdForespResponse = InfotrygdForesp().apply {
+                hovedStatus = StatusType().apply {
+                    kodeMelding = "00"
+                }
+            }
+
+            val receivedSykmelding = receivedSykmelding(
+                id = UUID.randomUUID().toString(),
+                sykmelding = sykmelding,
+                fellesformat = ""
+            )
+
+            val ruleMetadata = RuleMetadata(
+                receivedDate = receivedSykmelding.mottattDato,
+                signatureDate = receivedSykmelding.sykmelding.signaturDato,
+                patientPersonNumber = receivedSykmelding.personNrPasient,
+                rulesetVersion = receivedSykmelding.rulesetVersion,
+                legekontorOrgnr = receivedSykmelding.legekontorOrgNr,
+                tssid = receivedSykmelding.tssid,
+                infotrygdForesp = infotrygdForespResponse
+            )
+
+            val result = ruleTree.runRules(sykmelding, ruleMetadata)
+
+            result.first.rulePath.map { it.rule to it.ruleResult } shouldBeEqualTo listOf(
+                ValidationRules.NUMBER_OF_TREATMENT_DAYS_SET to false,
+                ValidationRules.GRADERT_REISETILSKUDD_ER_OPPGITT to true
+            )
+
+            mapOf(
+                "perioder" to sykmelding.perioder
+            ) shouldBeEqualTo result.first.ruleInputs
+
+            result.first.treeResult.ruleHit shouldBeEqualTo ValidationRuleHit.GRADERT_REISETILSKUDD_ER_OPPGITT.ruleHit
+
+            result.first.treeResult.status shouldBeEqualTo Status.MANUAL_PROCESSING
+        }
+
+        test("should trigger rule TRAVEL_SUBSIDY_SPECIFIED, should be MANUAL_PROCESSING") {
+            val sykmelding = generateSykmelding(
+                perioder = listOf(
+                    generatePeriode(
+                        fom = LocalDate.now(),
+                        tom = LocalDate.now().plusDays(10),
+                        reisetilskudd = true
+                    )
+                )
+            )
+
+            val infotrygdForespResponse = InfotrygdForesp().apply {
+                hovedStatus = StatusType().apply {
+                    kodeMelding = "00"
+                }
+            }
+
+            val receivedSykmelding = receivedSykmelding(
+                id = UUID.randomUUID().toString(),
+                sykmelding = sykmelding,
+                fellesformat = ""
+            )
+
+            val ruleMetadata = RuleMetadata(
+                receivedDate = receivedSykmelding.mottattDato,
+                signatureDate = receivedSykmelding.sykmelding.signaturDato,
+                patientPersonNumber = receivedSykmelding.personNrPasient,
+                rulesetVersion = receivedSykmelding.rulesetVersion,
+                legekontorOrgnr = receivedSykmelding.legekontorOrgNr,
+                tssid = receivedSykmelding.tssid,
+                infotrygdForesp = infotrygdForespResponse
+            )
+
+            val result = ruleTree.runRules(sykmelding, ruleMetadata)
+
+            result.first.rulePath.map { it.rule to it.ruleResult } shouldBeEqualTo listOf(
+                ValidationRules.NUMBER_OF_TREATMENT_DAYS_SET to false,
+                ValidationRules.GRADERT_REISETILSKUDD_ER_OPPGITT to false,
+                ValidationRules.TRAVEL_SUBSIDY_SPECIFIED to true
+            )
+
+            mapOf(
+                "perioder" to sykmelding.perioder
+            ) shouldBeEqualTo result.first.ruleInputs
+
+            result.first.treeResult.ruleHit shouldBeEqualTo ValidationRuleHit.TRAVEL_SUBSIDY_SPECIFIED.ruleHit
+
+            result.first.treeResult.status shouldBeEqualTo Status.MANUAL_PROCESSING
+        }
+
     }
 })
