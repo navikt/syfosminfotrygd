6fdfc4d2a04005c412f2c36c3a41656c643aae6f
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-09-05 14:29:48 +0200

    chore: drop /ca-bundle.jks

--- src/main/kotlin/no/nav/syfo/mq/MqTlsUtils.kt
+++ src/main/kotlin/no/nav/syfo/mq/MqTlsUtils.kt
@@ -7,7 +7,7 @@ class MqTlsUtils {
         fun getMqTlsConfig(): Properties {
             return Properties().also {
                 val mqEnv = MQEnvironment()
-                it["javax.net.ssl.keyStore"] = mqEnv.NAV_TRUSTSTORE_PATH + "/ca-bundle.jks"
+                it["javax.net.ssl.keyStore"] = mqEnv.NAV_TRUSTSTORE_PATH
                 it["javax.net.ssl.keyStorePassword"] = mqEnv.NAV_TRUSTSTORE_PASSWORD
                 it["javax.net.ssl.keyStoreType"] = "jks"
             }
